806920bf05898d2cd198a580948a70e68246fd4f
JuniorRivasTorres <67280424+JuniorRivasTorres@users.noreply.github.com>
2021-06-14 16:27:32 -0400

    firts

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from "../components/layout/layout"
+import Seo from "../components/layout/seo"
+import HeroSection from "../components/sections/HeroSection"
+
+function IndexPage () {
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <HeroSection />
+    </Layout> 
+  )
+}
+export default IndexPage
